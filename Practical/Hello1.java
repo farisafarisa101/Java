e228816e026dafd6955ebed6e28eaabfd1b8bc06
farisafarisa101 <99545974+farisafarisa101@users.noreply.github.com>
2023-01-20 20:09:37 +0530

    Add files via upload

---
+++ Practical/Hello1.java
@@ -0,0 +1,11 @@
+class My{
+	public static void display(){//static method 
+		System.out.println("Hi......");
+	}
+}
+class Hello1{
+	public static void main(String args[]){//entry point
+		System.out.println("Its My first program");//will print on console
+		My.display();//classname.methodname
+	}
+}//end of class
