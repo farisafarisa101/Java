f82f2992a1eef90c62de304fb909e01dd0a556c1
farisafarisa101 <99545974+farisafarisa101@users.noreply.github.com>
2023-02-20 18:27:12 +0530

    Add files via upload

---
+++ Practical/SortedArray.java
@@ -0,0 +1,26 @@
+package array;
+
+import java.util.Arrays;
+
+
+public class SortedArray {
+	 public static void showArray(){
+	 
+		//declare, initilize
+		int arr1[]= {10,9,8,7,6,5,4,3,2,1};
+		//print 
+		Arrays.sort(arr1);//sort method of array class
+		System.out.println("Sorted Array is ");//
+		for(int i=0;i<arr1.length;i++) {//printing array element
+						System.out.print(" "+arr1[i]);
+						}
+						
+			} 
+	
+
+	public static void main(String[] args) {
+		showArray();
+
+	}
+
+}
