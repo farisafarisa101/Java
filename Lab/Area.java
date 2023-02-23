dbfca6c7910da6c95df8994e000332a24d8cff47
farisafarisa101 <99545974+farisafarisa101@users.noreply.github.com>
2023-02-23 19:54:11 +0530

    Add files via upload

---
+++ Lab/Area.java
@@ -0,0 +1,11 @@
+package shapes;//package declaration
+
+public class Area {//defining class area 
+
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		Circle c=new Circle(8);//constructor invoked here 
+
+	}
+
+}
